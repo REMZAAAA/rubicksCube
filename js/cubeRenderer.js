711f086aa8b23497fe7631a0c6102df09c00bf12
remzaaaa <hamzachamrouk7@gmail.com>
2026-07-24 21:37:19 +0200

    render le cube dans la console (debug)

---
+++ js/cubeRenderer.js
@@ -0,0 +1,34 @@
+// import { cubeMap } from "./cubeMap"
+
+function printLineList(list=null, y=0){
+    let m = "";
+
+    if (!list) {
+        for (let i = 0; i < 3; i++) {
+            m += "    "
+        } return m
+    }
+
+    for (let i = 0; i < 3; i++) {
+        let color = list[i+(3 * y)].color.charAt(0);
+        let cube = parseInt(list[i+(3*y)].cube);
+        m += `${color}${cube}${(cube / 10) >= 1 ? "": " "} `;
+        
+    } return m
+}
+
+export function renderMap(list) {
+    console.log(
+        `${printLineList()}${printLineList(list[0], 0)}${printLineList()}`,
+        `\n${printLineList()}${printLineList(list[0], 1)}${printLineList()}`,
+        `\n${printLineList()}${printLineList(list[0], 2)}${printLineList()}`,
+        `\n${printLineList(list[3], 0)}${printLineList(list[1], 0)}${printLineList(list[2], 0)}`,
+        `\n${printLineList(list[3], 1)}${printLineList(list[1], 1)}${printLineList(list[2], 1)}`,
+        `\n${printLineList(list[3], 2)}${printLineList(list[1], 2)}${printLineList(list[2], 2)}`,
+        `\n${printLineList()}${printLineList(list[4], 0)}${printLineList()}`,
+        `\n${printLineList()}${printLineList(list[4], 1)}${printLineList()}`,
+        `\n${printLineList()}${printLineList(list[4], 2)}${printLineList()}`,
+        `\n${printLineList()}${printLineList(list[5], 0)}${printLineList()}`,
+        `\n${printLineList()}${printLineList(list[5], 1)}${printLineList()}`,
+        `\n${printLineList()}${printLineList(list[5], 2)}${printLineList()}`)
+}
