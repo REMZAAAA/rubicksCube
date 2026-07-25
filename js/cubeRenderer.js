713f4d7825156688724076e44d84377e474a7c81
remzaaaa <hamzachamrouk7@gmail.com>
2026-07-25 21:29:05 +0200

    changement de disposition de la map pour plus de compréhension

--- js/cubeRenderer.js
+++ js/cubeRenderer.js
@@ -15,16 +15,17 @@ function printLineList(list=null, y=0){
 
 export function renderMap(list) {
     console.log(
-        `${printLineList()}${printLineList(list[0], 0)}${printLineList()}`,
-        `\n${printLineList()}${printLineList(list[0], 1)}${printLineList()}`,
-        `\n${printLineList()}${printLineList(list[0], 2)}${printLineList()}`,
-        `\n${printLineList(list[3], 0)}${printLineList(list[1], 0)}${printLineList(list[2], 0)}`,
-        `\n${printLineList(list[3], 1)}${printLineList(list[1], 1)}${printLineList(list[2], 1)}`,
-        `\n${printLineList(list[3], 2)}${printLineList(list[1], 2)}${printLineList(list[2], 2)}`,
-        `\n${printLineList()}${printLineList(list[4], 0)}${printLineList()}`,
+        `${printLineList()}${printLineList(list[4], 0)}${printLineList()}`,
         `\n${printLineList()}${printLineList(list[4], 1)}${printLineList()}`,
         `\n${printLineList()}${printLineList(list[4], 2)}${printLineList()}`,
+        `\n${printLineList()}${printLineList(list[1], 0)}${printLineList()}`,
+        `\n${printLineList()}${printLineList(list[1], 1)}${printLineList()}`,
+        `\n${printLineList()}${printLineList(list[1], 2)}${printLineList()}`,
+        `\n${printLineList(list[3], 0)}${printLineList(list[0], 0)}${printLineList(list[2], 0)}`,
+        `\n${printLineList(list[3], 1)}${printLineList(list[0], 1)}${printLineList(list[2], 1)}`,
+        `\n${printLineList(list[3], 2)}${printLineList(list[0], 2)}${printLineList(list[2], 2)}`,
         `\n${printLineList()}${printLineList(list[5], 0)}${printLineList()}`,
         `\n${printLineList()}${printLineList(list[5], 1)}${printLineList()}`,
-        `\n${printLineList()}${printLineList(list[5], 2)}${printLineList()}`)
+        `\n${printLineList()}${printLineList(list[5], 2)}${printLineList()}`
+    )
 }
