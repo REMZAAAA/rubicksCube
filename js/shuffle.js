79d6026230ad2555374195edf8d9847636f42dc0
remzaaaa <hamzachamrouk7@gmail.com>
2026-08-02 20:51:41 +0200

    changement de la variable animation

--- js/shuffle.js
+++ js/shuffle.js
@@ -3,7 +3,6 @@ import { animationDuration } from "./cubeRotation.js";
 // All available move buttons.
 // Used to generate random scramble sequences.
 export const btnList = document.querySelectorAll("#moves button");
-const tempAnim = animationDuration;
 
 function wait(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -14,14 +13,12 @@ export async function shuffleCube(){
     for (let i = 0; i < 20; i++) {
         // Select a random move from the available buttons.
         const randomNumber = Math.floor(Math.random() * btnList.length);
-        // Disable animations during the scramble
-        // to make it complete instantly.
         document.documentElement.style.setProperty("--animation-duration", 0) // no animation for shuffle.
         // Trigger the move using the same logic
         // as a normal button click.
         btnList[randomNumber].click();
-        await wait(animationDuration);
+        await wait(0);
     }
     // Restore the original animation duration.
-    document.documentElement.style.setProperty("--animation-duration", tempAnim / 1000)
+    document.documentElement.style.setProperty("--animation-duration", animationDuration / 1000)
 }
