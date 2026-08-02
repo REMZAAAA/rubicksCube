2cdfa13b7a071d3cf1b240c966938ad212966ff2
remzaaaa <hamzachamrouk7@gmail.com>
2026-08-02 22:07:16 +0200

    changement de la logique du mix, on utilise maintenant la fonction executeMove() qui par défaut permet pas l'animation, on update quand même les stickers

--- js/shuffle.js
+++ js/shuffle.js
@@ -1,8 +1,7 @@
-import { animationDuration } from "./cubeRotation.js";
-
-// All available move buttons.
-// Used to generate random scramble sequences.
-export const btnList = document.querySelectorAll("#moves button");
+import { cubeMap, updateBackground } from "./cubeMap.js";
+import { moves } from "./moveHistory.js";
+import { executeMove } from "./cubeRotation.js";
+import { history, historyBar } from "./script.js";
 
 function wait(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -12,13 +11,14 @@ export async function shuffleCube(){
     // Generate a 20-move scramble.
     for (let i = 0; i < 20; i++) {
         // Select a random move from the available buttons.
-        const randomNumber = Math.floor(Math.random() * btnList.length);
-        document.documentElement.style.setProperty("--animation-duration", 0) // no animation for shuffle.
-        // Trigger the move using the same logic
-        // as a normal button click.
-        btnList[randomNumber].click();
+        const randomNumber = Math.floor(Math.random() * moves.length);
+
+        const move = [Object.keys(moves)[
+            Math.floor(Math.random() * Object.keys(moves).length)
+        ]];
+        executeMove(move, cubeMap, history, historyBar)
+        updateBackground(cubeMap, true);
         await wait(0);
     }
-    // Restore the original animation duration.
-    document.documentElement.style.setProperty("--animation-duration", animationDuration / 1000)
+    console.log("randomly mixed cubeMap:", cubeMap)
 }
