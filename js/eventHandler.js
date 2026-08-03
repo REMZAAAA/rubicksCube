c83321d8ad28e6c10a97fe3c99a42983df845a9e
remzaaaa <hamzachamrouk7@gmail.com>
2026-08-03 22:59:40 +0200

    création d'un nouveau fichier module eventHandler.js pour déplacer les events encombrant de main.js à eventHandler.js

---
+++ js/eventHandler.js
@@ -0,0 +1,74 @@
+import { executeMoves, animationDuration } from "./cubeRotation.js";
+import { resetMap } from "./cubeMap.js";
+import { resetHistory } from "./moveHistory.js";
+import { shuffleCube } from "./shuffle.js";
+import { updateColor, resetColor } from "./colors.js";
+import { mainCube } from "./layerHandler.js"
+
+const btnList = document.querySelectorAll("#moves button");
+btnList.forEach(element => {
+    element.addEventListener("click", () => {
+        const moveName = element.textContent;
+
+        // execute the move.
+        executeMoves(moveName, cubeMap, history, historyBar, true)
+
+        // Prevent move spam while an animation
+        // is currently playing.
+        btnList.forEach(el => {
+            el.disabled = true;
+            setTimeout(() => {
+                el.disabled = false;
+            }, animationDuration * 1)
+        });
+        
+    });
+});
+
+const resetEL = document.querySelector("#commands .reset")
+resetEL.addEventListener("click", () => {
+    // Restore the cube to its solved state
+    // and clear the move history.
+    resetMap();
+    resetHistory(history, historyBar);
+});
+
+const shuffleEl = document.querySelector("#commands .shuffle")
+shuffleEl.addEventListener("click", async () => {
+    // Perform a random shuffle sequence.
+    await shuffleCube();
+});
+
+let resolveMode = false;
+
+const resolveEl = document.querySelector("#commands .resolve")
+resolveEl.addEventListener("click", () => {
+    resolveMode = !resolveMode;
+    document.querySelector("#moves .slider").style.visibility = resolveMode ? "hidden" : "visible";
+});
+
+const colorInputEl = document.querySelectorAll(".colors input");
+colorInputEl.forEach(element => {
+    // Update cube colors whenever the user
+    // changes a color picker.
+    element.addEventListener("change", () => {
+        updateColor();
+    });
+});
+
+const debugInput = document.querySelector("#menu .debug input");
+debugInput.addEventListener("click", () => {
+    // Debug mode displays cube IDs
+    // directly on the cube.
+    if (debugInput.checked){
+        mainCube.classList.add("showCell");
+    } else{
+        mainCube.classList.remove("showCell");
+    }
+})
+
+const resetColorEl = document.querySelector("#menu .resetColor button")
+resetColorEl.addEventListener("click", () => {
+    // Restore the default cube color scheme.
+    resetColor();
+});
