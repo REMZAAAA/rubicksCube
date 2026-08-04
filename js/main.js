9ebdcb987cf3bf3328e959c1308f98972df7ae54
remzaaaa <hamzachamrouk7@gmail.com>
2026-08-04 15:02:59 +0200

    passage en commentaire des tests, réarangement du code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { mainCube, layer } from "./layerHandler.js"
+import { layer } from "./layerHandler.js"
 import { cubeMap, getPieceByFaceId } from "./cubeMap.js"
 import { setCross } from "./algorithm.js"
 import { renderMap } from "./cubeRenderer.js"
@@ -6,28 +6,26 @@ import { createMoves } from "./history.js"
 
 export const history = Array();
 export const historyPanel = document.getElementById("history");
+export const mainCube = document.getElementById("mainCube");
 
 export function doCrossAlgo(){
     setCross();
 }
-
 window.doCrossAlgo = doCrossAlgo;
 
-// The temporary rotation layer must be attached
-// to the cube before any move can be animated.
+// const tests = [
+//     getPieceByFaceId("corner", [0, 1, 2], cubeMap),
+//     getPieceByFaceId("corner", [0, 1], cubeMap),
+//     getPieceByFaceId("corner", [0], cubeMap),
+//     getPieceByFaceId("edge"  , [0, 1], cubeMap),
+//     getPieceByFaceId("edge"  , [0], cubeMap),
+//     getPieceByFaceId("center", [0], cubeMap),
+// ]
+
+// console.log(tests)
+
 mainCube.appendChild(layer);
 
 createMoves();
 
 renderMap(cubeMap);
-
-const tests = [
-    getPieceByFaceId("corner", [0, 1, 2], cubeMap),
-    getPieceByFaceId("corner", [0, 1], cubeMap),
-    getPieceByFaceId("corner", [0], cubeMap),
-    getPieceByFaceId("edge"  , [0, 1], cubeMap),
-    getPieceByFaceId("edge"  , [0], cubeMap),
-    getPieceByFaceId("center", [0], cubeMap),
-]
-
-console.log(tests)
